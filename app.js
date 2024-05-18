69ff4a789f2293e10963028c77612ffcb3a84d19
Matin Schams <schams.matin@gmail.com>
2024-05-17 19:18:40 -0700

    Create a global error handler in app.js

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.use(bodyParser.json());
 
 app.use('/api/user', userRoutes);
 
+app.use((err, req, res, next) => {
+  const globalError = {
+    log: 'Global Error Found',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, globalError, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
