009bb94af21a023883fb6aefe833c5e630a9f946
Matin Schams <schams.matin@gmail.com>
2024-05-17 22:30:33 -0700

    Setup associations for friendships

--- models/userModel.js
+++ models/userModel.js
@@ -1,25 +1,34 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      // Correct associations with aliases
+      User.belongsToMany(models.User, {
+        as: 'Friends',
+        through: models.Friendship,
+        foreignKey: 'userId',
+        otherKey: 'friendId',
+      });
+      User.belongsToMany(models.User, {
+        as: 'FriendOf',
+        through: models.Friendship,
+        foreignKey: 'friendId',
+        otherKey: 'userId',
+      });
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    },
+  );
   return User;
 };
